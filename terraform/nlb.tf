5fda1f1f697505ca5d8f6b80cfa3dbbe2d21de88
yongquanhuang <49548493+yongquanhuang@users.noreply.github.com>
2020-06-16 17:23:51 +0800

    Update nlb.tf

--- terraform/nlb.tf
+++ terraform/nlb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "billing" {
   name               = "billing-lb"
   internal           = false
   load_balancer_type = "network"
-  subnets            = [aws_subnet.private.*.id]
+  subnets            = [join(",", aws_subnet.private.*.id)]
 
   enable_deletion_protection = true
 
@@ -32,8 +32,36 @@ resource "aws_lb_target_group" "billing-https" {
 
 
 resource "aws_lb_target_group_attachment" "billing-https" {
+  count          = length(var.availability_zones)
   target_group_arn = aws_lb_target_group.billing-https.arn
-  target_id        = data.aws_instance.web.id
+  target_id        = aws_instance.web[count.index].id
   port             = 443
 }
 
+
+resource "aws_lb_listener" "billing-http" {
+  load_balancer_arn = aws_lb.billing.arn
+  port              = "80"
+  protocol          = "tcp"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.billing-http.arn
+  }
+}
+
+
+resource "aws_lb_target_group" "billing-http" {
+  name     = "billing-http-lb"
+  port     = 80
+  protocol = "tcp"
+  vpc_id   = aws_vpc.billing.id
+}
+
+
+resource "aws_lb_target_group_attachment" "billing-http" {
+  count          = length(var.availability_zones)
+  target_group_arn = aws_lb_target_group.billing-http.arn
+  target_id        = aws_instance.web[count.index].id
+  port             = 80
+}
